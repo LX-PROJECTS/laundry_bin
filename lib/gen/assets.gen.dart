aeff582f7eb41378f4b7757d76ae5e2e48bb955f
RisanPT <risanpallithazha@gmail.com>
2024-06-28 11:19:35 +0530

    Order Page Started

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,401 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/ic_+(pickup point).svg
+  String get icPickupPoint => 'assets/icons/ic_+(pickup point).svg';
+
+  /// File path: assets/icons/ic_+plus(washing-page).svg
+  String get icPlusWashingPage => 'assets/icons/ic_+plus(washing-page).svg';
+
+  /// File path: assets/icons/ic_-minus(Washing-page).svg
+  String get icMinusWashingPage => 'assets/icons/ic_-minus(Washing-page).svg';
+
+  /// File path: assets/icons/ic_Ellipse 126.svg
+  String get icEllipse126 => 'assets/icons/ic_Ellipse 126.svg';
+
+  /// File path: assets/icons/ic_FAQ(profile-page).svg
+  String get icFAQProfilePage => 'assets/icons/ic_FAQ(profile-page).svg';
+
+  /// File path: assets/icons/ic_Hoodies(Washing-page).svg
+  String get icHoodiesWashingPage =>
+      'assets/icons/ic_Hoodies(Washing-page).svg';
+
+  /// File path: assets/icons/ic_Jeans(Washing-page).svg
+  String get icJeansWashingPage => 'assets/icons/ic_Jeans(Washing-page).svg';
+
+  /// File path: assets/icons/ic_Shorts(Washing-page).svg
+  String get icShortsWashingPage => 'assets/icons/ic_Shorts(Washing-page).svg';
+
+  /// File path: assets/icons/ic_Star-Rating(Blue).svg
+  String get icStarRatingBlue => 'assets/icons/ic_Star-Rating(Blue).svg';
+
+  /// File path: assets/icons/ic_T-shirt(Washing-page).svg
+  String get icTShirtWashingPage => 'assets/icons/ic_T-shirt(Washing-page).svg';
+
+  /// File path: assets/icons/ic_aboutus(Profile-page).svg
+  String get icAboutusProfilePage =>
+      'assets/icons/ic_aboutus(Profile-page).svg';
+
+  /// File path: assets/icons/ic_address(profile-page).svg
+  String get icAddressProfilePage =>
+      'assets/icons/ic_address(profile-page).svg';
+
+  /// File path: assets/icons/ic_arrow-left(for message sending).svg
+  String get icArrowLeftForMessageSending =>
+      'assets/icons/ic_arrow-left(for message sending).svg';
+
+  /// File path: assets/icons/ic_arrow-left.svg
+  String get icArrowLeft => 'assets/icons/ic_arrow-left.svg';
+
+  /// File path: assets/icons/ic_arrow-right(FAQ).svg
+  String get icArrowRightFAQ => 'assets/icons/ic_arrow-right(FAQ).svg';
+
+  /// File path: assets/icons/ic_arrow-right(Washing-page).svg
+  String get icArrowRightWashingPage =>
+      'assets/icons/ic_arrow-right(Washing-page).svg';
+
+  /// File path: assets/icons/ic_arrow-right(splash-screen).svg
+  String get icArrowRightSplashScreen =>
+      'assets/icons/ic_arrow-right(splash-screen).svg';
+
+  /// File path: assets/icons/ic_building(location).svg
+  String get icBuildingLocation => 'assets/icons/ic_building(location).svg';
+
+  /// File path: assets/icons/ic_calendar(DOB).svg
+  String get icCalendarDOB => 'assets/icons/ic_calendar(DOB).svg';
+
+  /// File path: assets/icons/ic_calendar(tracking-page).svg
+  String get icCalendarTrackingPage =>
+      'assets/icons/ic_calendar(tracking-page).svg';
+
+  /// File path: assets/icons/ic_camera.svg
+  String get icCamera => 'assets/icons/ic_camera.svg';
+
+  /// File path: assets/icons/ic_card(credit card).svg
+  String get icCardCreditCard => 'assets/icons/ic_card(credit card).svg';
+
+  /// File path: assets/icons/ic_clock(slot).svg
+  String get icClockSlot => 'assets/icons/ic_clock(slot).svg';
+
+  /// File path: assets/icons/ic_close-circle.svg
+  String get icCloseCircle => 'assets/icons/ic_close-circle.svg';
+
+  /// File path: assets/icons/ic_contact-us(profile-page).svg
+  String get icContactUsProfilePage =>
+      'assets/icons/ic_contact-us(profile-page).svg';
+
+  /// File path: assets/icons/ic_coupon-discont.svg
+  String get icCouponDiscont => 'assets/icons/ic_coupon-discont.svg';
+
+  /// File path: assets/icons/ic_dashboard(profile-page).svg
+  String get icDashboardProfilePage =>
+      'assets/icons/ic_dashboard(profile-page).svg';
+
+  /// File path: assets/icons/ic_delivery-truck(tracking-page).svg
+  String get icDeliveryTruckTrackingPage =>
+      'assets/icons/ic_delivery-truck(tracking-page).svg';
+
+  /// File path: assets/icons/ic_edit(pen).svg
+  String get icEditPen => 'assets/icons/ic_edit(pen).svg';
+
+  /// File path: assets/icons/ic_edit-pen(Black).svg
+  String get icEditPenBlack => 'assets/icons/ic_edit-pen(Black).svg';
+
+  /// File path: assets/icons/ic_edit-pen(Edit-profile).svg
+  String get icEditPenEditProfile =>
+      'assets/icons/ic_edit-pen(Edit-profile).svg';
+
+  /// File path: assets/icons/ic_empty-wallet(COD).svg
+  String get icEmptyWalletCOD => 'assets/icons/ic_empty-wallet(COD).svg';
+
+  /// File path: assets/icons/ic_floor-number.svg
+  String get icFloorNumber => 'assets/icons/ic_floor-number.svg';
+
+  /// File path: assets/icons/ic_fluent_eye(auth-screen).svg
+  String get icFluentEyeAuthScreen =>
+      'assets/icons/ic_fluent_eye(auth-screen).svg';
+
+  /// File path: assets/icons/ic_google-pay.svg
+  String get icGooglePay => 'assets/icons/ic_google-pay.svg';
+
+  /// File path: assets/icons/ic_jacket(Washing-page).svg
+  String get icJacketWashingPage => 'assets/icons/ic_jacket(Washing-page).svg';
+
+  /// File path: assets/icons/ic_location-blue.svg
+  String get icLocationBlue => 'assets/icons/ic_location-blue.svg';
+
+  /// File path: assets/icons/ic_location-pin.svg
+  String get icLocationPin => 'assets/icons/ic_location-pin.svg';
+
+  /// File path: assets/icons/ic_mastercard.svg
+  String get icMastercard => 'assets/icons/ic_mastercard.svg';
+
+  /// File path: assets/icons/ic_normal-wind-2.svg
+  String get icNormalWind2 => 'assets/icons/ic_normal-wind-2.svg';
+
+  /// File path: assets/icons/ic_phone-pay.svg
+  String get icPhonePay => 'assets/icons/ic_phone-pay.svg';
+
+  /// File path: assets/icons/ic_privacy-policy(profile-page).svg
+  String get icPrivacyPolicyProfilePage =>
+      'assets/icons/ic_privacy-policy(profile-page).svg';
+
+  /// File path: assets/icons/ic_review(profile-page).svg
+  String get icReviewProfilePage => 'assets/icons/ic_review(profile-page).svg';
+
+  /// File path: assets/icons/ic_scented.svg
+  String get icScented => 'assets/icons/ic_scented.svg';
+
+  /// File path: assets/icons/ic_search-normal.svg
+  String get icSearchNormal => 'assets/icons/ic_search-normal.svg';
+
+  /// File path: assets/icons/ic_shirt(Washing-page).svg
+  String get icShirtWashingPage => 'assets/icons/ic_shirt(Washing-page).svg';
+
+  /// File path: assets/icons/ic_skirt(Washing-page).svg
+  String get icSkirtWashingPage => 'assets/icons/ic_skirt(Washing-page).svg';
+
+  /// File path: assets/icons/ic_star-yellow.svg
+  String get icStarYellow => 'assets/icons/ic_star-yellow.svg';
+
+  /// File path: assets/icons/ic_tele-phone(location).svg
+  String get icTelePhoneLocation => 'assets/icons/ic_tele-phone(location).svg';
+
+  /// File path: assets/icons/ic_telephone(Blue).svg
+  String get icTelephoneBlue => 'assets/icons/ic_telephone(Blue).svg';
+
+  /// File path: assets/icons/ic_tick(offers screen).svg
+  String get icTickOffersScreen => 'assets/icons/ic_tick(offers screen).svg';
+
+  /// File path: assets/icons/ic_total-booking.svg
+  String get icTotalBooking => 'assets/icons/ic_total-booking.svg';
+
+  /// File path: assets/icons/ic_truck(Tracking-page).svg
+  String get icTruckTrackingPage => 'assets/icons/ic_truck(Tracking-page).svg';
+
+  /// File path: assets/icons/ic_upi.svg
+  String get icUpi => 'assets/icons/ic_upi.svg';
+
+  /// File path: assets/icons/ic_visa.svg
+  String get icVisa => 'assets/icons/ic_visa.svg';
+
+  /// File path: assets/icons/ic_washing(Tracking-page).svg
+  String get icWashingTrackingPage =>
+      'assets/icons/ic_washing(Tracking-page).svg';
+
+  /// File path: assets/icons/icon_Star-Rating.svg
+  String get iconStarRating => 'assets/icons/icon_Star-Rating.svg';
+
+  /// File path: assets/icons/icon_arrow-down(homescreen).svg
+  String get iconArrowDownHomescreen =>
+      'assets/icons/icon_arrow-down(homescreen).svg';
+
+  /// File path: assets/icons/icon_calendar(homescreen).svg
+  String get iconCalendarHomescreen =>
+      'assets/icons/icon_calendar(homescreen).svg';
+
+  /// File path: assets/icons/icon_cold(homescreen).svg
+  String get iconColdHomescreen => 'assets/icons/icon_cold(homescreen).svg';
+
+  /// File path: assets/icons/icon_dryclean(homescreen).svg
+  String get iconDrycleanHomescreen =>
+      'assets/icons/icon_dryclean(homescreen).svg';
+
+  /// File path: assets/icons/icon_homescreenHouse.svg
+  String get iconHomescreenHouse => 'assets/icons/icon_homescreenHouse.svg';
+
+  /// File path: assets/icons/icon_hot(homescreen).svg
+  String get iconHotHomescreen => 'assets/icons/icon_hot(homescreen).svg';
+
+  /// File path: assets/icons/icon_iron(homescreen).svg
+  String get iconIronHomescreen => 'assets/icons/icon_iron(homescreen).svg';
+
+  /// File path: assets/icons/icon_menubar(homescreen).svg
+  String get iconMenubarHomescreen =>
+      'assets/icons/icon_menubar(homescreen).svg';
+
+  /// File path: assets/icons/icon_notification(homescreen).svg
+  String get iconNotificationHomescreen =>
+      'assets/icons/icon_notification(homescreen).svg';
+
+  /// File path: assets/icons/icon_offer-tag(homescreen).svg
+  String get iconOfferTagHomescreen =>
+      'assets/icons/icon_offer-tag(homescreen).svg';
+
+  /// File path: assets/icons/icon_onboardiron.svg
+  String get iconOnboardiron => 'assets/icons/icon_onboardiron.svg';
+
+  /// File path: assets/icons/icon_onbordshirt.svg
+  String get iconOnbordshirt => 'assets/icons/icon_onbordshirt.svg';
+
+  /// File path: assets/icons/icon_onbordwashing.svg
+  String get iconOnbordwashing => 'assets/icons/icon_onbordwashing.svg';
+
+  /// File path: assets/icons/icon_splash.svg
+  String get iconSplash => 'assets/icons/icon_splash.svg';
+
+  /// File path: assets/icons/icon_successmark.svg
+  String get iconSuccessmark => 'assets/icons/icon_successmark.svg';
+
+  /// File path: assets/icons/icon_tick-circle(homescreen).svg
+  String get iconTickCircleHomescreen =>
+      'assets/icons/icon_tick-circle(homescreen).svg';
+
+  /// File path: assets/icons/icon_user(homescreen).svg
+  String get iconUserHomescreen => 'assets/icons/icon_user(homescreen).svg';
+
+  /// File path: assets/icons/icon_washing(homescreen).svg
+  String get iconWashingHomescreen =>
+      'assets/icons/icon_washing(homescreen).svg';
+
+  /// File path: assets/icons/icon_washing+iron(homescreen).svg
+  String get iconWashingIronHomescreen =>
+      'assets/icons/icon_washing+iron(homescreen).svg';
+
+  /// List of all assets
+  List<String> get values => [
+        icPickupPoint,
+        icPlusWashingPage,
+        icMinusWashingPage,
+        icEllipse126,
+        icFAQProfilePage,
+        icHoodiesWashingPage,
+        icJeansWashingPage,
+        icShortsWashingPage,
+        icStarRatingBlue,
+        icTShirtWashingPage,
+        icAboutusProfilePage,
+        icAddressProfilePage,
+        icArrowLeftForMessageSending,
+        icArrowLeft,
+        icArrowRightFAQ,
+        icArrowRightWashingPage,
+        icArrowRightSplashScreen,
+        icBuildingLocation,
+        icCalendarDOB,
+        icCalendarTrackingPage,
+        icCamera,
+        icCardCreditCard,
+        icClockSlot,
+        icCloseCircle,
+        icContactUsProfilePage,
+        icCouponDiscont,
+        icDashboardProfilePage,
+        icDeliveryTruckTrackingPage,
+        icEditPen,
+        icEditPenBlack,
+        icEditPenEditProfile,
+        icEmptyWalletCOD,
+        icFloorNumber,
+        icFluentEyeAuthScreen,
+        icGooglePay,
+        icJacketWashingPage,
+        icLocationBlue,
+        icLocationPin,
+        icMastercard,
+        icNormalWind2,
+        icPhonePay,
+        icPrivacyPolicyProfilePage,
+        icReviewProfilePage,
+        icScented,
+        icSearchNormal,
+        icShirtWashingPage,
+        icSkirtWashingPage,
+        icStarYellow,
+        icTelePhoneLocation,
+        icTelephoneBlue,
+        icTickOffersScreen,
+        icTotalBooking,
+        icTruckTrackingPage,
+        icUpi,
+        icVisa,
+        icWashingTrackingPage,
+        iconStarRating,
+        iconArrowDownHomescreen,
+        iconCalendarHomescreen,
+        iconColdHomescreen,
+        iconDrycleanHomescreen,
+        iconHomescreenHouse,
+        iconHotHomescreen,
+        iconIronHomescreen,
+        iconMenubarHomescreen,
+        iconNotificationHomescreen,
+        iconOfferTagHomescreen,
+        iconOnboardiron,
+        iconOnbordshirt,
+        iconOnbordwashing,
+        iconSplash,
+        iconSuccessmark,
+        iconTickCircleHomescreen,
+        iconUserHomescreen,
+        iconWashingHomescreen,
+        iconWashingIronHomescreen
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/img_Ellipse 90(booking-page).svg
+  String get imgEllipse90BookingPage =>
+      'assets/images/img_Ellipse 90(booking-page).svg';
+
+  /// File path: assets/images/img_Ellipse 91(booking-page).svg
+  String get imgEllipse91BookingPage =>
+      'assets/images/img_Ellipse 91(booking-page).svg';
+
+  /// File path: assets/images/img_Tracking-page.svg
+  String get imgTrackingPage => 'assets/images/img_Tracking-page.svg';
+
+  /// File path: assets/images/img_auth_background.svg
+  String get imgAuthBackground => 'assets/images/img_auth_background.svg';
+
+  /// File path: assets/images/img_bg_recepit.svg
+  String get imgBgRecepit => 'assets/images/img_bg_recepit.svg';
+
+  /// File path: assets/images/img_offer(homescreen).svg
+  String get imgOfferHomescreen => 'assets/images/img_offer(homescreen).svg';
+
+  /// File path: assets/images/img_onbord.svg
+  String get imgOnbord => 'assets/images/img_onbord.svg';
+
+  /// File path: assets/images/img_shirt(profile-page).svg
+  String get imgShirtProfilePage => 'assets/images/img_shirt(profile-page).svg';
+
+  /// File path: assets/images/img_washing-machine(booking-page).svg
+  String get imgWashingMachineBookingPage =>
+      'assets/images/img_washing-machine(booking-page).svg';
+
+  /// File path: assets/images/img_washing-page.svg
+  String get imgWashingPage => 'assets/images/img_washing-page.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        imgEllipse90BookingPage,
+        imgEllipse91BookingPage,
+        imgTrackingPage,
+        imgAuthBackground,
+        imgBgRecepit,
+        imgOfferHomescreen,
+        imgOnbord,
+        imgShirtProfilePage,
+        imgWashingMachineBookingPage,
+        imgWashingPage
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:laundry_bin/gen/assets.gen.dart';
 import 'package:laundry_bin/extension.dart';
 import 'package:laundry_bin/gen/assets.gen.dart';
 import 'package:laundry_bin/l10n/gen/app_localizations.dart';
